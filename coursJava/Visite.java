1463347d62ba258e8dd9a037c5b44ed4c9e57045
gnorgol <52493523+gnorgol@users.noreply.github.com>
2021-04-20 10:54:28 +0200

    Add files via upload

---
+++ coursJava/Visite.java
@@ -0,0 +1,55 @@
+package coursJava;
+public class Visite {
+	
+	 String etudiants;
+	 String date;
+	 int note;
+	 
+	 
+	 public Visite(String etudiants, String date ,int note ) 
+	 {
+		 this.etudiants = etudiants;
+		 this.date = date;
+		 this.note = note;
+		 
+	 }
+	 String getEtudiants() {
+		 
+		 return this.etudiants;
+		 
+		 
+	 }
+	 String getDate() {
+		 
+		 return this.date;
+		 
+		 
+	 }
+	 void setEtudiant(String etudiant) {
+		 this.etudiants = etudiants; 
+		 
+	 }
+	 void setDate(String date) {
+		 this.date = date; 
+		 
+	 }
+	 void setEtudiant(int note) {
+		 this.note = note; 
+	 }
+	 int getNote() {
+	 
+	 return this.note;
+	 
+	 
+}
+	 public static void main(String[] arg) {
+		 
+		 Visite V1 = new Visite("","",0);
+		 Visite V2 = new Visite("Arthur","12-12-2012",17);
+		 System.out.println(V1.etudiants);
+		 System.out.println(V2.etudiants);
+		 
+	 }
+	
+	
+}
